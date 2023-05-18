17492aa3ad54ae8868cf63c3422bbc5305bdde8e
6Whistle <camjun0506@icloud.com>
2023-05-18 10:41:20 +0900

    2023-05-18 level 2

---
+++ level_2/2-H-index.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+    for(int i = citations.size() - 1; i >= 0; i--)
+        if(citations[i] > i)    return i;
+    return 0;
+}
