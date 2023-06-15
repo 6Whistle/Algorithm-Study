2c9ceba45d070f3daa30426908a0be1ce59e5b88
6Whistle <camjun0506@icloud.com>
2023-06-15 15:14:59 +0900

    2023-06-15 level 2

---
+++ level_2/2-괄호_회전하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int check_string(string &s, int begin, int size){
+    string st;
+    int sub_cnt = 0;
+    for(int i = begin; i < begin + size; i++){
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')   st.push_back(s[i]);
+        else if(s[i] == (st.back() + 1) || s[i] == (st.back() + 2))     st.pop_back();
+        else    return 0;
+        if(st.empty())  sub_cnt++;
+    }
+    return (st.empty()) ? sub_cnt : 0;
+}
+
+int solution(string s) {
+    s += s;
+    for(int i = 0, size = s.size() >> 1, answer; i < size; i++)    if(answer = check_string(s, i, size))   return answer;
+    return 0;
+}
