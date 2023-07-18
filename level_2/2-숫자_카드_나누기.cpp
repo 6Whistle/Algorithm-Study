23809b9f90698ff6b9e2475517c5879c53e6fb62
6Whistle <camjun0506@icloud.com>
2023-07-18 11:55:31 +0900

    2023-07-18 level 2

--- level_2/2-숫자_카드_나누기.cpp
+++ level_2/2-숫자_카드_나누기.cpp
@@ -9,7 +9,6 @@ bool check_div(int a, vector<int> &arr){    for(int i = 0; i < arr.size(); i++) 
 
 int solution(vector<int> arrayA, vector<int> arrayB) {
     int gcd[2] = { arrayA[0], arrayB[0] }, answer = 0;
-    vector<int> gcd_common[2];
     for_each(arrayA.begin() + 1, arrayA.end(), [&](int i){   gcd[0] = find_gcd(gcd[0], i);   });
     for_each(arrayB.begin() + 1, arrayB.end(), [&](int i){   gcd[1] = find_gcd(gcd[1], i);   });
     for(int i = gcd[0]; i > 0; i--)    if(!(gcd[0] % i) && check_div(i, arrayB)){   answer = i;     break;  }
