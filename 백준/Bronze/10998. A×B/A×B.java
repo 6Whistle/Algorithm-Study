c4174b9d156cc2b796fac52fc6ea2ffeb57b9745
6Whistle <78692304+6Whistle@users.noreply.github.com>
2024-03-26 19:19:10 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println(scanner.nextInt() * scanner.nextInt());
+    }
+}
